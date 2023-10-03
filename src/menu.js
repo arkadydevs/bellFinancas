89b2c4c3be69f5471808a47e01237b5cecb3bc4f
JoaoAHaupt <joao.haupt@aluno.ifsp.edu.br>
2023-10-02 22:36:42 -0300

    Update carrosel

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,10 @@
-const urlMicrosoft = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&interval=5min&apikey=3E887CSJSW1LMFS4';
-
+/*const urlMicrosoft = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&interval=5min&apikey=3E887CSJSW1LMFS4';
+    
 fetch(urlMicrosoft)
     .then(response => response.json())
     .then(data => {
         const dataAtual = new Date();
-        dataAtual.setDate(dataAtual.getDate() - 2);
+        dataAtual.setDate(dataAtual.getDate());
         const ano = dataAtual.getFullYear();
         const mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
         const dia = String(dataAtual.getDate()).padStart(2, '0');
@@ -16,30 +16,23 @@ fetch(urlMicrosoft)
         const dadosDiaAnterior = timeSeries[dataDiaAnterior];
 
         if (dadosDiaAnterior) {
-            const openPrice = dadosDiaAnterior['1. open'];
-            const highPrice = dadosDiaAnterior['2. high'];
-            const lowPrice = dadosDiaAnterior['3. low'];
+
             const closePrice = dadosDiaAnterior['4. close'];
             const volume = dadosDiaAnterior['5. volume'];
 
-            console.log(`Dados do dia anterior (data: ${dataDiaAnterior}):`);
-            console.log(`Preço de Abertura: ${openPrice}`);
-            console.log(`Preço Máximo: ${highPrice}`);
-            console.log(`Preço Mínimo: ${lowPrice}`);
-            console.log(`Preço de Fechamento: ${closePrice}`);
-            console.log(`Volume: ${volume}`);
-
-            // Aqui você pode fazer qualquer processamento adicional ou atualizar elementos na sua aplicação front-end
+                        
+            const spanValorMicrosoft = document.getElementsByName("valor-da-microsoft");
+            if (spanValorMicrosoft.length > 0) {
+                spanValorMicrosoft.forEach(element => {
+                    element.textContent = `Valor MSFT: $${closePrice}`;
+                });
+            }
         } else {
             console.log(`Dados do dia anterior não encontrados para ${dataDiaAnterior}`);
         }
     })
     .catch(error => {
         console.error('Erro ao buscar dados:', error);
-    });
+    });*/
 
-    const spanValorMicrosoft = document.getElementsByName("valor-da-microsoft");
-    spanValorMicrosoft.forEach(element => {
-        element.textContent = openPrice; 
-    });
-        
+    
